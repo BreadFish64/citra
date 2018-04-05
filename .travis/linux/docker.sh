42bc386e326c6e7581dcbb3fbe5ddedfd45e4ef5
BreadFish64 <mohror64@gmail.com>
2018-04-05 14:13:59 -0500

    meep

--- .travis/linux/docker.sh
+++ .travis/linux/docker.sh
@@ -9,7 +9,7 @@ apt-get install -y build-essential libsdl2-dev qtbase5-dev libqt5opengl5-dev qtt
 wget https://cmake.org/files/v3.10/cmake-3.10.1-Linux-x86_64.sh
 echo y | sh cmake-3.10.1-Linux-x86_64.sh --prefix=cmake
 export PATH=/citra/cmake/cmake-3.10.1-Linux-x86_64/bin:$PATH
-
+git submodule update --init --recursive
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DENABLE_QT_TRANSLATION=ON -DCITRA_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"}
 make -j4
