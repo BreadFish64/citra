2bba878cedb94a01da06bec62f28a84664655927
BreadFish64 <mohror64@gmail.com>
2018-12-31 15:48:53 -0600

    android: edit strings

--- src/android/app/src/main/java/org/citra_emu/citra/ui/main/MainActivity.java
+++ src/android/app/src/main/java/org/citra_emu/citra/ui/main/MainActivity.java
@@ -5,14 +5,40 @@
 package org.citra_emu.citra.ui.main;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import org.citra_emu.citra.R;
 
 public final class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
