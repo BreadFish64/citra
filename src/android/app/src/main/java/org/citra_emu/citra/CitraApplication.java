6d374d23cf5f9aba9aec0e3e4fb854055922228d
BreadFish64 <mohror64@gmail.com>
2019-01-15 19:13:11 -0600

    crap

--- src/android/app/src/main/java/org/citra_emu/citra/CitraApplication.java
+++ src/android/app/src/main/java/org/citra_emu/citra/CitraApplication.java
@@ -5,5 +5,20 @@
 package org.citra_emu.citra;
 
 import android.app.Application;
+import android.content.Context;
 
-public class CitraApplication extends Application {}
+public class CitraApplication extends Application {
+    private static CitraApplication citraApplication;
+
+    static {System.loadLibrary("citra-android");}
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        citraApplication = this;
+    }
+
+    public static Context getAppContext(){ return citraApplication.getApplicationContext();}
+
+
+}
